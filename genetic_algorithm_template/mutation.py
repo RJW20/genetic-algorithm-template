9ed1a1f8adbd2ea5f7e9d6906cbb5f2df3629252
Robert Watts <robertjwatts84@outlook.com>
2024-02-16 22:00:41 +0000

    Add gaussian and uniform mutations

---
+++ genetic_algorithm_template/mutation.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from .genome import Genome
+
+
+def gaussian_mutation(chromosome: Genome, mutation_rate: float) -> None:
+    """Perform a gaussian mutation for each gene in a Genome with probability mutation_rate.
+    
+    Each gene selected for mutation will have a value ~N(0,0.2) added to it.
+    """
+
+    for layer in chromosome.layers:
+        for arr in [layer.bias, layer.weights]:
+                mutation = np.random.normal(size=arr.shape) * 0.2
+                mask = np.random.random(size=arr.shape) < mutation_rate
+                arr[mask] += mutation[mask]
+
+
+def uniform_mutation(chromosome: Genome, mutation_rate: float) -> None:
+    """Randomly mutate each gene in a Genome with probability mutation_rate.
+    
+    Each gene selected for mutation will be assigned a new value ~U[-1,1].
+    """
+
+    for layer in chromosome.layers:
+        for arr in [layer.bias, layer.weights]:
+                mutation = np.random.uniform(-1, 1, size=arr.shape)
+                mask = np.random.random(size=arr.shape) < mutation_rate
+                arr[mask] += mutation[mask]
