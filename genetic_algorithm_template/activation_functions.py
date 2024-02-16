cd644d601364ab23e1aa6db93e3785f9e5a900d7
Robert Watts <robertjwatts84@outlook.com>
2024-02-16 19:08:34 +0000

    add activation_by_name

--- genetic_algorithm_template/activation_functions.py
+++ genetic_algorithm_template/activation_functions.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import numpy as np
 
 
@@ -9,3 +11,13 @@ def softmax(X: np.array) -> np.array:
     return np.array([1/sum(np.exp(np.subtract(X, _))) for _ in np.nditer(X)])
 
 linear = lambda X: X
+
+
+def activation_by_name(name: str) -> Callable[[np.array], np.array]:
+    """Return activation function from name."""
+
+    activations = [('sigmoid', sigmoid), ('relu', relu), ('softmax', softmax), ('linear', linear)]
+    func = [activation[1] for activation in activations if activation[0].lower() == name.lower()]
+    assert len(func) == 1, f"invalid activation function {name}"
+
+    return func[0]
