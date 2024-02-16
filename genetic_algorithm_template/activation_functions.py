36273377f61ce64be028ed98791f7194d27b02c2
Robert Watts <robertjwatts84@outlook.com>
2024-02-16 18:54:45 +0000

    modify activation functions

--- genetic_algorithm_template/activation_functions.py
+++ genetic_algorithm_template/activation_functions.py
@@ -1,15 +1,11 @@
-from typing import NewType, Callable
-
 import numpy as np
 
 
-def softmax_definition(X: np.array) -> np.array:
+sigmoid = lambda X: 1.0 / (1.0 + np.exp(-X))
+
+relu = lambda X: np.maximum(0, X)
+
+def softmax(X: np.array) -> np.array:
     return np.array([1/sum(np.exp(np.subtract(X, _))) for _ in np.nditer(X)])
 
-
-ActivationFunction = NewType('ActivationFunction', Callable[[np.array], np.array])
-
-sigmoid = ActivationFunction(lambda X: 1.0 / (1.0 + np.exp(-X)))
-relu = ActivationFunction(lambda X: np.maximum(0, X))
-softmax = ActivationFunction(softmax_definition)
-linear = ActivationFunction(lambda X: X)
+linear = lambda X: X
