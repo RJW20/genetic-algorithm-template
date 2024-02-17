45c9222510fc0fab10cb17e640c1723b84bee9e5
Robert Watts <robertjwatts84@outlook.com>
2024-02-17 16:10:55 +0000

    Include player arguments in settings

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 
 
-settings = {
+player_args = {}
+
+genetic_algorithm_settings = {
 
     #population properties
     'population_size': 1000,        #number of players in the population
@@ -10,7 +12,7 @@ settings = {
     'total_generations': 500,       #number of generations to run to
 
     #genome properties
-    'structure': ((24, 'none'), (16, 'sgd'), (3, 'softmax')),
+    'structure': ((24, ), (16, 'sigmoid'), (3, 'softmax')),    #options for activation are ['sigmoid', 'relu', 'softmax', 'linear']
 
     #evolution properties
     'parent_percentage': 0.2,       #percentage of parents to repopulate the next generation from
