3cf2a196858e89018780bf4ab41b07a80c1716c2
Robert Watts <robertjwatts84@outlook.com>
2024-02-17 15:11:27 +0000

    Added player example

--- example/player.py
+++ example/player.py
@@ -1,72 +1,51 @@
-from abc import ABC, abstractmethod
-from __future__ import annotations
 from typing import Any
 
-from .genome import Genome
+from genetic_algorithm import BasePlayer
 
-class BasePlayer(ABC):
-    """Abstract base class for a Player in a population."""
+
+class Player(BasePlayer):
+    """Example of a Player class.
+    
+    All these methods must be present.
+    """
 
     def __init__(self) -> None:
         pass
 
-    @property
-    def fitness(self) -> float:
-        return self._fitness
-
-    @fitness.setter
-    def fitness(self, value: float) -> None:
-        self._fitness = value
-
-    @property
-    def genome(self) -> Genome:
-        return self._genome
-
-    @genome.setter
-    def genome(self, value: Genome) -> None:
-        self._genome = value
-
-    @abstractmethod
     def look(self) -> None:
         """Update the attributes used as input to the Genome."""
         pass
 
-    @abstractmethod
     def think(self) -> Any:
         """Feed the input into the Genome and turn the output into a valid move."""
         pass
 
-    @abstractmethod
     def move(self, move: Any) -> None:
         """Advance to the state achieved by carrying out move."""
         pass
 
     @property
-    @abstractmethod
     def is_dead(self) -> bool:
         """Return True if the player has reached a state where the game is over."""
         pass
 
-    def __eq__(self, other: BasePlayer) -> bool:
-        return self.genome == other.genome
-    
-    @abstractmethod
     def __getstate__(self) -> dict:
         """Return a dictionary containing attribute names and their values as (key, value) pairs.
         
         All values must also be pickleable i.e. not use __slots__ or have __getstate__ and __setstate__ methods like this.
+        If this class uses __slots__ or extends one that does this must be changed.
         """
-        pass
 
-    @abstractmethod
+        return self.__dict__
+
     def __setstate__(self, d: dict) -> BasePlayer:
-        """Load the attributes in the dictionary d into self."""
-        pass
+        """Load the attributes in the dictionary d into self.
+        
+        If this class uses __slots__ or extends one that does this must be changed.
+        """
 
-    @abstractmethod
+        self.__dict__ = d
+
     def empty_clone(self) -> BasePlayer:
         """Return a new instance of self's class without a genome."""
         pass
-
-
-
