3239f1497d7f51be8dc5be1279057711869a9b5a
Robert Watts <robertjwatts84@outlook.com>
2024-02-19 10:34:20 +0000

    Change activation functions from lambda to def

--- genetic_algorithm/activation_functions.py
+++ genetic_algorithm/activation_functions.py
@@ -3,14 +3,17 @@ from typing import Callable, Literal
 import numpy as np
 
 
-sigmoid = lambda X: 1.0 / (1.0 + np.exp(-X))
+def sigmoid(X: np.ndarray) -> np.ndarray:
+    return 1.0 / (1.0 + np.exp(-X))
 
-relu = lambda X: np.maximum(0, X)
+def relu(X: np.ndarray) -> np.ndarray:
+    return np.maximum(0, X)
 
 def softmax(X: np.ndarray) -> np.ndarray:
     return np.array([1/sum(np.exp(np.subtract(X, _))) for _ in np.nditer(X)])
 
-linear = lambda X: X
+def linear(X: np.ndarray) -> np.ndarray:
+    return X
 
 def activation_by_name(name: Literal['sigmoid', 'relu', 'softmax', 'linear']) -> Callable[[np.ndarray], np.ndarray]:
     """Return activation function from name."""
