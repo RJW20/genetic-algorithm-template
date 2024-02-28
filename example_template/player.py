3bdf9fd225612010a299cdb684194f19800d53dc
RJW20 <robertjwatts84@outlook.com>
2024-02-28 17:11:18 +0000

    Remove __get/setstate__ methods from BasePlayer

--- example_template/player.py
+++ example_template/player.py
@@ -35,23 +35,6 @@ class Player(BasePlayer):
         """Put player in a state to begin simulation in its environment."""
         self.score = 0
 
-    def __getstate__(self) -> dict:
-        """Return a dictionary containing attribute names and their values as (key, value) pairs.
-        
-        All values must also be pickleable i.e. not use __slots__ or have __getstate__ and __setstate__ methods like this.
-        If this class uses __slots__ or extends one that does this must be changed.
-        """
-
-        return self.__dict__
-
-    def __setstate__(self, d: dict) -> BasePlayer:
-        """Load the attributes in the dictionary d into self.
-        
-        If this class uses __slots__ or extends one that does this must be changed.
-        """
-
-        self.__dict__ = d
-
     def empty_clone(self) -> BasePlayer:
         """Return a new instance of self's class without a genome."""
         pass
